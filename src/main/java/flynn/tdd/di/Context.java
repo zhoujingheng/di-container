e32aec652cef446a0b61d762133cdf67238de4c5
zhoujingheng <294046665@qq.com>
2022-04-17 13:51:18 +0800

    refactor

--- src/main/java/flynn/tdd/di/Context.java
+++ src/main/java/flynn/tdd/di/Context.java
@@ -1,29 +1,30 @@
 package flynn.tdd.di;
 
+import jakarta.inject.Provider;
+
 import java.util.HashMap;
 import java.util.Map;
 
 public class Context {
-    private Map<Class<?>, Object> components = new HashMap<>();
-    private Map<Class<?>, Class<?>> componentImplementations = new HashMap<>();
+    private Map<Class<?>, Provider<?>> providers = new HashMap<>();
 
     public <ComponentType> void bind(Class<ComponentType> type, ComponentType instance) {
-        components.put(type, instance);
-    }
-
-    public <ComponentType> ComponentType get(Class<ComponentType> type) {
-        if (components.containsKey(type))
-            return (ComponentType) components.get(type);
-        Class<?> implementation = componentImplementations.get(type);
-        try {
-            return (ComponentType) implementation.getConstructor().newInstance();
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
+        providers.put(type, (Provider<ComponentType>) () -> instance);
     }
 
     public <ComponentType, ComponentImplementation extends ComponentType>
     void bind(Class<ComponentType> type, Class<ComponentImplementation> implementation) {
-        componentImplementations.put(type, implementation);
+        providers.put(type, (Provider<ComponentType>) () -> {
+            try {
+                return (ComponentType) ((Class<?>) implementation).getConstructor().newInstance();
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        });
     }
+
+    public <ComponentType> ComponentType get(Class<ComponentType> type) {
+        return (ComponentType) providers.get(type).get();
+    }
+
 }
