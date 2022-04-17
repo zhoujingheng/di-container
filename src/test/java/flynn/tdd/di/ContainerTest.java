92d840e3c07a601b955d689a50df55744064f5f8
zhoujingheng <294046665@qq.com>
2022-04-17 13:09:14 +0800

    任务分解

--- src/test/java/flynn/tdd/di/ContainerTest.java
+++ src/test/java/flynn/tdd/di/ContainerTest.java
@@ -1,4 +1,40 @@
 package flynn.tdd.di;
 
+import org.junit.jupiter.api.Nested;
+
 public class ContainerTest {
+
+    @Nested
+    public class ComponentConstruction {
+        //TODO instance
+        //TODO abstract class
+        //TODO interface
+
+        @Nested
+        public class ConstructorInjection {
+            //TODO instance
+            //TODO with dependencies
+            //TODO A->B->C
+        }
+
+        @Nested
+        public class FiledInjection {
+
+        }
+
+        @Nested
+        public class MethodInjection {
+
+        }
+    }
+
+    @Nested
+    public class DependenciesSelection {
+
+    }
+
+    @Nested
+    public class LifecycleManagement {
+
+    }
 }
